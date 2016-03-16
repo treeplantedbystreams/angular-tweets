89203340ee082ff29e4be3270265477363e6121a
treeplantedbystreams <davbros88@hotmail.com>
2016-03-16 12:45:25 -0500

    initial commit

---
+++ client/js/main.js
@@ -0,0 +1,11 @@
+
+var controllers = angular.module('myApp', [])
+	controllers.controller('MyController', ['$scope', function($scope){
+        $scope.welcomeMessage="twits who tweet"
+
+	}])
+	.config(['', function(){
+
+
+}]);
+         
